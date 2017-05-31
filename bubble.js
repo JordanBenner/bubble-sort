88bb6483b46813d0a866df67784ea6a152ef374b
Jordan Benner <jordanbenner@outlook.com>
2017-05-30 19:10:33 -0500

    Bubble sort rev1

--- bubble.js
+++ bubble.js
@@ -1,20 +1,20 @@
 var r = [10, 2, 3, 9, 7, 6, 8, 1, 5, 4]
 
-function bubbleSort(data) {
-  var swapped = false;
+function bubbleSort(a) {
+  var swapped;
   do {
-    for (var ndx = 0; ndx < data.length; ndx++) {
-      if (data[ndx] > data[ndx + 1]) {
-        var temp = data[ndx];
-        data[ndx + 1] = temp;
+    swapped = false;
+    for (var i = 0; i < data.length; i++) {
+      if (a[i] > a[i+1]) {
+        var temp = a[i];
+        a[i+1] = temp;
         swapped = true;
-        console.log(data);
+
       }
     }
-  } console.log(a);
+   console.log(a);
 } while (!swapped);
 
-
-console.log(a);
-bubbleSort(a);
-console.log(a};
+console.log(data);
+bubbleSort(data);
+console.log(data);
